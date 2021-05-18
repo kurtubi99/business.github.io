f6890c12dcf1bb038c50f92e691c30d76be3efdf
Kurtubi Juanaid <68976701+kurtubi99@users.noreply.github.com>
2021-05-18 23:32:16 +0600

    My Portfolio Website
    This is my practice portfolio website

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$('document').ready(function () {
+	// Typed JS
+	var typed = new Typed('.typed', {
+		strings: ['Kurtubi Juanaid'],
+		typeSpeed: 70,
+		startDelay: 300,
+		// loop: true,
+		showCursor: false,
+		// smartBackspace: true,
+		// fadeOutClass: 'typed-fade-out',
+	});
+});
